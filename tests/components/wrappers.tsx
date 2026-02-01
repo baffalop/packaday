39f883e5580c23f5c4e899cbf10a5f3518f5779b
Nikita Gaidakov <ngaidakov@podfather.com>
2026-02-01 10:11:04 +0000

    Fix most ts issues

--- tests/components/wrappers.tsx
+++ tests/components/wrappers.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// @ts-expect-error Melange output has no type declarations
 import { make as BoardComponent, makeProps } from '../../_build/default/src/output/src/Board.js'
 
 export function Board () {
---
+++ types.d.ts
@@ -0,0 +1,2 @@
+// vite-plugin-melange has no type declarations
+declare module 'vite-plugin-melange'
