9be3c01ff2c1ba1af43bcc752fbb4f03bbe23aac
Nikita Gaidakov <ngaidakov@podfather.com>
2026-02-01 16:36:23 +0000

    Revert "Temp: remove Piece.Button test"
    This reverts commit 53e4e5e958187d9842a5c9db792c3f9e9029c3c6.

--- tests/components/wrappers.tsx
+++ tests/components/wrappers.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 // @ts-expect-error Melange output has no type declarations
 import { make as BoardComponent, makeProps } from '../../src/Board.js'
+// @ts-expect-error Melange output has no type declarations
+import { Button } from '../../src/Piece.js'
 
 export function Board () {
   return React.createElement(BoardComponent, makeProps(undefined, undefined))
+}
+
+export function PieceButton ({ t }: { t: string }) {
+  return React.createElement(Button.make, Button.makeProps(t, undefined))
 }
