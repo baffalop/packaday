3b236d2360cc63699c3f0c5431b69e58c160841a
Nikita Gaidakov <ngaidakov@podfather.com>
2026-02-01 06:51:30 +0000

    Add back melange plugin, necessary for node_modules/melange resolution

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import melange from 'vite-plugin-melange'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    melange({
+      emitDir: 'src',
+      buildTarget: 'output',
+      buildCommand: 'opam exec -- dune build @app',
+      watchCommand: 'opam exec -- dune build --watch @app',
+    }),
     tailwindcss(),
   ],
   server: {
