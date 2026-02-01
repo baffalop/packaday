51898bc8612d4b27e4b92a90bf75fc1dd4ff47dc
Nikita Gaidakov <ngaidakov@podfather.com>
2026-02-01 22:49:35 +0000

    Extract Piece.Panel

--- src/Piece.mli
+++ src/Piece.mli
@@ -24,3 +24,8 @@ module Button : sig
   val makeProps : t:t -> ?key:string -> unit -> < t:t > Js.t
   val make : < t:t > Js.t -> React.element
 end
+
+module Panel : sig
+  val makeProps : ?key:string -> unit -> <  > Js.t
+  val make : <  > Js.t -> React.element
+end
