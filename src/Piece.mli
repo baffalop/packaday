717b30b216a043836d7c4e5114a275f1e12eafc7
Nikita Gaidakov <ngaidakov@podfather.com>
2026-02-01 22:46:22 +0000

    Implement Piece shapes

--- src/Piece.mli
+++ src/Piece.mli
@@ -9,6 +9,17 @@ type t = [
   | `Z
 ]
 
+type segment = O | H
+type shape_spec = segment Array.t Array.t
+
+val spec_of_t : t -> shape_spec
+
+module Shape : sig
+  val cell_size : int
+  val makeProps : t:t -> ?key:string -> unit -> < t:t > Js.t
+  val make : < t:t > Js.t -> React.element
+end
+
 module Button : sig
   val makeProps : t:t -> ?key:string -> unit -> < t:t > Js.t
   val make : < t:t > Js.t -> React.element
