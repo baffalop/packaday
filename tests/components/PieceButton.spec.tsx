53e4e5e958187d9842a5c9db792c3f9e9029c3c6
Nikita Gaidakov <ngaidakov@podfather.com>
2026-02-01 16:30:26 +0000

    Temp: remove Piece.Button test

--- tests/components/PieceButton.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/experimental-ct-react'
-import { PieceButton } from './wrappers'
-
-test.describe('Piece.Button Component', () => {
-  test('renders button', async ({ mount }) => {
-    const component = await mount(<PieceButton t='Thumb' />)
-    await expect(component).toBeVisible()
-  })
-})
